6664b1709916858100384852334b0a098438469d
Pedro Sousa <pedroteosousa@gmail.com>
2020-12-31 15:02:03 -0300

    Improve noise reduction
    Moved noise reduction to it's own function that receives an image and
    returns a (hopefully) noiseless image.
    
    It checks every component looking if it's in range of reasonable height,
    width and pixel count. This values are hardcode for now (will probably
    remain hardcode, but in another file).
    
    Also, using os.path to make image save path work for everybody.

--- Clash.py
+++ Clash.py
@@ -3,6 +3,11 @@ import pytesseract
 from PIL import Image
 import time
 import pyscreenshot as ImageGrab
+import os
+
+COMPONENT_SIZE_RANGE = (40, 180)
+HEIGHT_RANGE = (11, 15)
+WIDTH_RANGE = (2, 14)
 
 custom_config = r'-c tessedit_char_whitelist=0123456789 --psm 6'
 
@@ -48,42 +53,79 @@ height = 120
 
 screen_rect = [ x, y, width, height ]
 
-p = {}
-w = {}
+def remove_noise(image):
+    parent = {}
+    component = {}
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-def reset():
-    global p, w
-    p = {}
-    w = {}
+    width, height = image.size
     for i in range(0,width):
         for j in range(0,height):
-            p[(i,j)] = (i,j)
-            w[(i,j)] = 1
+            parent[(i, j)] = (i, j)
+            component[(i, j)] = [(i, j)]
 
-adj = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    def find(x):
+        if parent[x] == x:
+            return x
+        parent[x] = find(parent[x])
+        return parent[x]
 
-def find(x):
-    global p
-    if p[x] == x:
-        return x
-    p[x] = find(p[x])
-    return p[x]
+    def union(a, b):
+        a = find(a)
+        b = find(b)
+        if a == b:
+            return
+        if len(component[a]) < len(component[b]):
+            a, b = b, a
+        parent[b] = a
+        for pixel in component[b]:
+            component[a].append(pixel)
+        component[b] = []
 
-def union(a, b):
-    global p, w
-    a = find(a)
-    b = find(b)
-    if a == b:
-        return
-    if w[a] < w[b]:
-        a, b = b, a
-    w[a] += w[b]
-    p[b] = a
+    def get_component_size(pixel):
+        pixel = find(pixel)
+        comp = component[pixel]
+        minx, maxx = width, 0
+        miny, maxy = height, 0
+        for pix in comp:
+            minx = min(minx, pix[0])
+            maxx = max(maxx, pix[0])
+            miny = min(miny, pix[1])
+            maxy = max(maxy, pix[1])
+        return maxx - minx, maxy - miny, len(comp)
+
+    def in_range(value, test_range):
+        return test_range[0] <= value <= test_range[1]
+
+    # create components looking at directions vector
+    for i in range(width):
+        for j in range(height):
+            color = image.getpixel((i, j))
+            if color[0] == 0:
+                for k in range(len(directions)):
+                    x, y = directions[k]
+                    if 0 <= i+x < width and 0 <= j+y < height:
+                        n_color = image.getpixel((i+x, j+y))
+                        if n_color[0] == 0:
+                            union((i, j), (i+x, j+y))
+    # find a representative for each component
+    roots = set()
+    for i in range(width):
+        for j in range(height):
+            color = image.getpixel((i, j))
+            if color[0] == 0:
+                roots.add(find((i, j)))
+    # paint white all too-small or too-big pixels, in any category
+    for root in roots:
+        wd, hg, sz = get_component_size(root)
+        if not in_range(hg, HEIGHT_RANGE) or not in_range(wd, WIDTH_RANGE) or not in_range(sz, COMPONENT_SIZE_RANGE):
+            for pixel in component[root]:
+                image.putpixel(pixel, (255,255,255))
+    return image
 
 while ( True ):
     image = screenGrab( screen_rect )        # Grab the area of the screen
 
-    reset()
     for i in range(0,width):
         for j in range(0,height):
             current_color = image.getpixel( (i,j) )
@@ -91,30 +133,10 @@ while ( True ):
                 image.putpixel( (i,j), (0,0,0))
             else:
                 image.putpixel( (i,j), (255,255,255))
-    for i in range(0,width):
-        for j in range(0,height):
-            color = image.getpixel((i, j))
-            if color[0] == 0:
-                for k in range(len(adj)):
-                    x, y = adj[k]
-                    if 0 <= i+x < width and 0 <= j+y < height: 
-                        n_color = image.getpixel((i+x, j+y))
-                        if n_color[0] == 0:
-                            union((i, j), (i+x, j+y))
-    image.save( '/home/pedroteosousa/Downloads/screen_grob.png', 'PNG' )
-    t = {}
-    for i in range(0,width):
-        for j in range(0,height):
-            color = image.getpixel((i, j))
-            if color[0] == 0 and (w[find((i, j))] < 60 or w[find((i, j))] > 220):
-                image.putpixel( (i,j), (255,255,255))
-                t[find((i,j))] = w[find((i, j))]
-    for a in t:
-        pass
-        #print (t[a])
-
+    image.save(os.path.expanduser('~/Downloads/screen_grob.png'), 'PNG')
+    image = remove_noise(image)
+    image.save(os.path.expanduser('~/Downloads/screen_grab.png'), 'PNG')
     text = pytesseract.image_to_string( image, config=custom_config )   # OCR the image
-    image.save( '/home/pedroteosousa/Downloads/screen_grab.png', 'PNG' )
 
     text = text.strip()
     if ( len( text ) > 0 ):
