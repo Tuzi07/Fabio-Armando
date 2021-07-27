02400f61ee5366b4ea3bbe565ed68f0be9534aa0
Tuzi07 <artursm007.2@gmail.com>
2021-07-27 15:17:55 -0300

    Add loot type change suggestion to sample

--- config.sample.py
+++ config.sample.py
@@ -6,7 +6,9 @@ import recognizer
 message = "BIRL!"
 loot_type = "default"
 max_retries = 3
-search_limit = 30
+
+# search_limit = 30
+# skipped_villages = 0
 
 # runs once when program starts, can be used to change loot_type
 def setup():
@@ -30,6 +32,45 @@ class Loot:
     if loot_type == "eagle_unloaded":
       import eagle
       return gold > 500000 and elixir > 500000 and dark_elixir > 4000 and eagle.is_unloaded()
+
+#    should notify with loot type change suggestion
+
+#    def should_notify(loot):
+#    global skipped_villages
+#    skipped_villages+=1
+#    print("Vila " + str(skipped_villages) + "\n")
+#    if skipped_villages < search_limit:
+#      gold, elixir, dark_elixir = loot
+#      import eagle
+#      if loot_type == '610k':
+#        if gold > 610000 and elixir > 610000 and dark_elixir > 4800 and eagle.is_unloaded():
+#          skipped_villages = 0
+#          return True
+#      if loot_type == '550k':
+#        if gold > 550000 and elixir > 550000 and dark_elixir > 4300 and eagle.is_unloaded():
+#          skipped_villages = 0
+#          return True
+#      if loot_type == '500k':
+#        if gold > 500000 and elixir > 500000 and dark_elixir > 3900 and eagle.is_unloaded():
+#          skipped_villages = 0
+#          return True
+#      if loot_type == '450k':
+#        if gold > 450000 and elixir > 450000 and dark_elixir > 3500 and eagle.is_unloaded():
+#          skipped_villages = 0
+#          return True
+#      if loot_type == '400k':
+#        if gold > 400000 and elixir > 400000 and dark_elixir > 3150 and eagle.is_unloaded():
+#          skipped_villages = 0
+#          return True
+#      if loot_type == '305k':
+#        if gold > 305000 and elixir > 305000 and dark_elixir > 2400 and eagle.is_unloaded():
+#          skipped_villages = 0
+#          return True
+#    else:
+#      skipped_villages = 0
+#      engine.say("Decrease Loot Type")
+#      engine.runAndWait()
+#      setup()
 
   def notify(loot):
     engine.say(message)
